67aa9858dcee3007ec7860b0e51aadd8e54a635d
ikiwq <ikiigaiiii@gmail.com>
2024-07-11 23:12:51 +0200

    Initial setup

---
+++ sdk/core/src/main/java/com/lixega/ecommerce/sdk/core/model/dto/ExampleDTO.java
@@ -0,0 +1,10 @@
+package com.lixega.ecommerce.sdk.core.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ExampleDTO {
+    private String example;
+}
